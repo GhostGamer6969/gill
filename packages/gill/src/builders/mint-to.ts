1d929b0fb29ecfbea77ae0ff1bd04d53e1e86a58
GhostGamer6969 <arambhmani@hotmail.com>
2025-09-21 15:53:06 +0530

    fixing

--- packages/gill/src/builders/mint-to.ts
+++ packages/gill/src/builders/mint-to.ts
@@ -1,69 +1,27 @@
-import type { Instruction, TransactionSigner } from "@solana/kit";
+import type { Address, Instruction, TransactionSigner } from "@solana/kit";
+import { getAssociatedTokenAccountAddress, TOKEN_2022_PROGRAM_ADDRESS } from "../programs";
+import { getMintTokensInstructions } from "../programs/token/instructions/mint-tokens";
 
-import { getCreateAssociatedTokenIdempotentInstruction, getMintToInstruction } from "@solana-program/token-2022";
-import { checkedAddress, checkedTransactionSigner } from "../core";
-import { checkedTokenProgramAddress, getAssociatedTokenAccountAddress } from "../programs/token/addresses";
-import type { TokenInstructionBase } from "../programs/token/instructions/types";
-
-export type MintToIxArgs = TokenInstructionBase & {
-  /**
-   * The Owner address capable of authorizing minting of new tokens.
-   *
-   * - this should normally by a `TransactionSigner`
-   * - only for multi-sig authorities (like Squads Protocol), should you supply an `Address`
-   * */
-  /** Wallet address to receive the tokens being minted, via their associated token account (ata) */
-  toOwner: TransactionSigner ;
-
-  /** Amount of tokens to mint to the `owner` via their `ata` */
+type MintToArgs = {
+  mint: Address;
+  toOwner: TransactionSigner;
   amount: bigint | number;
 };
 
-/**
- * Create the instructions required to mint tokens to any wallet/owner,
- * including creating their ATA if it does not exist
- *
- * @example
- *
- * ```
- * const mint = await generateKeyPairSigner();
- * const destination = await generateKeyPairSigner();
- *
- * const instructions = mintTo({
- *   mint,
- *   toOwner: signer,
- *   amount: 1000, // note: be sure to consider the mint's `decimals` value
- *   // if decimals=2 => this will mint 10.00 tokens
- *   // if decimals=4 => this will mint 0.100 tokens
- *   // tokenProgram: TOKEN_PROGRAM_ADDRESS, // default
- *   // tokenProgram: TOKEN_2022_PROGRAM_ADDRESS,
- * });
- * ```
- */
-export async function mintToInstructions(args: MintToIxArgs): Promise<Instruction[]> {
-  args.tokenProgram = checkedTokenProgramAddress(args.tokenProgram);
-  args.feePayer = checkedTransactionSigner(args.feePayer);
-  args.mint = checkedAddress(args.mint);
+export async function mintTo({
+  mint,
+  toOwner,
+  amount,
+}: MintToArgs): Promise<Instruction[]> {
+  const ata = await getAssociatedTokenAccountAddress(mint, toOwner, TOKEN_2022_PROGRAM_ADDRESS);
 
-  return [
-    // create idempotent will gracefully fail if the ata already exists. this is the gold standard!
-    getCreateAssociatedTokenIdempotentInstruction({
-      owner: checkedAddress(args.toOwner),
-      mint: args.mint,
-      ata: await getAssociatedTokenAccountAddress(args.mint, args.toOwner, args.tokenProgram),
-      payer: args.feePayer,
-      tokenProgram: args.tokenProgram,
-    }),
-    getMintToInstruction(
-      {
-        mint: args.mint,
-        mintAuthority: args.toOwner,
-        token: await getAssociatedTokenAccountAddress(args.mint, args.toOwner, args.tokenProgram),
-        amount: args.amount,
-      },
-      {
-        programAddress: args.tokenProgram,
-      },
-    ),
-  ];
+  return getMintTokensInstructions({
+    mint,
+    feePayer: toOwner,          
+    mintAuthority: toOwner,
+    destination: toOwner,
+    ata,
+    amount,
+    tokenProgram: TOKEN_2022_PROGRAM_ADDRESS,
+  });
 }
